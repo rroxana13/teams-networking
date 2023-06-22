4bb34914f007c104b2cb25b7c2dfc36739bec192
Roxana <roxana.gherman@fidasolutions.com>
2023-06-22 20:35:12 +0300

    display teams with js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import "./style.css";
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function getTeamAsHtml(team) {
+  return `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
+    <td>❌ &#9998;</td>
+    </tr>`;
+}
+
+function displayTeams(teams) {
+  const teamsHTML = teams.map(getTeamAsHtml);
+
+  $("#teamsTable tbody").innerHTML = teamsHTML.join("");
+}
+
 function loadTeams() {
   fetch("teams.json")
     .then(r => r.json())
+
     .then(teams => {
-      console.warn("teams", teams);
+      displayTeams(teams);
     });
 }
 

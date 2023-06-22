e80dedaaf88439886b6c587260924417adcdf497
Roxana <roxana.gherman@fidasolutions.com>
2023-06-22 19:57:44 +0300

    create load teams request

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./style.css";
 
-console.warn("starting the app");
+function loadTeams() {
+  const request = fetch("teams.json");
+  console.warn("request", request);
+
+  const response = request.then(r => r.json());
+  console.warn("response", response);
+
+  const r2 = response.then(result => {
+    console.warn("result", result);
+  });
+  console.info("r2", r2);
+}
+
+loadTeams();

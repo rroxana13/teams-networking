82f920f8dcfab77da8196efaacb7a563d8ce182a
Roxana <roxana.gherman@fidasolutions.com>
2023-06-22 20:00:00 +0300

    cleanup consoles

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import "./style.css";
 
 function loadTeams() {
   const request = fetch("teams.json");
-  console.warn("request", request);
 
   const response = request.then(r => r.json());
-  console.warn("response", response);
 
-  const r2 = response.then(result => {
+  response.then(result => {
     console.warn("result", result);
   });
-  console.info("r2", r2);
 }
 
 loadTeams();

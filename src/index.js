9eb27c64fb465b8042e42299ac841214c6554590
Roxana <roxana.gherman@fidasolutions.com>
2023-06-28 10:57:36 +0300

    update team request

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+let editId;
 let allTeams = [];
 
 function $(selector) {
@@ -49,23 +50,28 @@ function deleteTeamRequest(id) {
   }).then(r => r.json());
 }
 
-function updateTeamRequest() {
-  fetch("http://localhost:3000/teams-json/update", {
+function updateTeamRequest(team) {
+  return fetch("http://localhost:3000/teams-json/update", {
     method: "PUT",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({
-      id: "fedcba1610310163146",
-      promotion: "WON3",
-      members: "UpdatedName",
-      name: "Name",
-      url: "https://github.com/nmatei/teams-networking"
-    })
-  });
+    body: JSON.stringify(team)
+  }).then(r => r.json());
+}
+
+function createTeamRequest(team) {
+  return fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(team)
+  }).then(r => r.json());
 }
 
 function startEdit(id) {
+  editId = id;
   const team = allTeams.find(team => team.id == id);
   console.info("start edit", team);
 
@@ -73,6 +79,28 @@ function startEdit(id) {
   $("#members").value = team.members;
   $("input[name=name]").value = team.name;
   $("input[name=url]").value = team.url;
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  const promotion = $("#promotion").value;
+  const members = $("#members").value;
+  const name = $("input[name=name]").value;
+  const url = $("input[name=url]").value;
+  const team = {
+    id: editId,
+    promotion,
+    members,
+    name: name,
+    url: url
+  };
+  console.warn("submit", team);
+  updateTeamRequest(team).then(status => {
+    if (status.success) {
+      // v.1
+      window.location.reload();
+    }
+  });
 }
 
 function initEvents() {
@@ -91,6 +119,7 @@ function initEvents() {
       startEdit(id);
     }
   });
+  $("#teamsForm").addEventListener("submit", onSubmit);
 }
 
 loadTeams();

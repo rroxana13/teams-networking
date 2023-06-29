651819ad8eb3be7c9294b49801728350b9c0256b
Roxana <roxana.gherman@fidasolutions.com>
2023-06-29 19:23:44 +0300

    implement search teams

--- src/index.js
+++ src/index.js
@@ -112,9 +112,19 @@ function onSubmit(e) {
 }
 
 function searchTeams(e) {
-  let searchText = e.target.value;
+  let searchText = e.target.value.toLowerCase();
   console.warn(searchText);
   console.warn(allTeams);
+  var filteredTeams = allTeams.filter(team => {
+    return (
+      team.promotion.toLowerCase().includes(searchText) ||
+      team.members.toLowerCase().includes(searchText) ||
+      team.url.toLowerCase().includes(searchText) ||
+      team.name.toLowerCase().includes(searchText)
+    );
+  });
+  console.warn("filteredTeams", filteredTeams);
+  displayTeams(filteredTeams);
 }
 
 function initEvents() {
